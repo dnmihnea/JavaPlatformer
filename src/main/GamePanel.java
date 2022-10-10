ee7bffc731e3df4ca5062c9f333dc5115095d98d
dnmihnea <mihnea.dinu100@gmail.com>
2022-10-10 17:46:28 +0100

    created basic game loop

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,6 +1,8 @@
 package main;
 
+import java.awt.Color;
 import java.awt.Graphics;
+import java.util.Random;
 
 import javax.swing.JPanel;
 
@@ -12,11 +14,22 @@ public class GamePanel extends JPanel{
 	private KeyboardInputs keyboardInputs;
 	private MouseInputs mouseInputs;
 	
-	private int xDelta = 100;
-	private int yDelta = 100;
+	private float xDelta = 100;
+	private float yDelta = 100;
+	
+	private float xDir = 1f;
+	private float yDir = 1f;
+	
+	private int frames = 0;
+	private long lastCheck = 0;
+	
+	private Color color = new Color(150, 20, 90);
+	
+	private Random random;
 
 	public GamePanel() {
 		
+		random = new Random();
 		keyboardInputs = new KeyboardInputs(this);
 		mouseInputs = new MouseInputs(this);
 		addKeyListener(keyboardInputs); //placed keyboard inputs in its own class
@@ -26,23 +39,44 @@ public class GamePanel extends JPanel{
 	
 	public void changeXDelta(int value) {
 		this.xDelta += value;
-		repaint();
 	}
 	
 	public void changeYDelta(int value) {
 		this.yDelta += value;
-		repaint();
 	}
 	
 	public void setRectPos (int x, int y) {
 		this.xDelta = x;
 		this.yDelta = y;
-		repaint();
 	}
 	
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		
-		g.fillRect(xDelta, yDelta, 200, 50);
+		updateRectangle();
+		g.setColor(color);
+		g.fillRect((int)xDelta, (int)yDelta, 200, 50);
+	}
+
+	private void updateRectangle() {
+		xDelta += xDir;
+		if(xDelta > 400 || xDelta < 0) {
+			xDir *= -1;
+			color = getRandomColor();
+		}
+		
+		yDelta += yDir;
+		if(yDelta > 400 || yDelta < 0) {
+			yDir *= -1;
+			color = getRandomColor();
+		}
+	}
+
+	private Color getRandomColor() {
+		int r = random.nextInt(255);
+		int g = random.nextInt(255);
+		int b = random.nextInt(255);
+		
+		return new Color(r,g,b);
 	}
 }
