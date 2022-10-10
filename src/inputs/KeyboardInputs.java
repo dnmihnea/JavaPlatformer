39cd13ade7a3c496c7b720f1a97fe88cddee7bf0
dnmihnea <mihnea.dinu100@gmail.com>
2022-10-10 16:54:42 +0100

    go fuck yourself

--- src/inputs/KeyboardInputs.java
+++ src/inputs/KeyboardInputs.java
@@ -3,12 +3,13 @@ package inputs;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
+import main.Game;
 import main.GamePanel;
 
-public class KeyboardInputs implements KeyListener{
-	
+public class KeyboardInputs implements KeyListener {
+
 	private GamePanel gamePanel;
-	
+
 	public KeyboardInputs(GamePanel gamePanel) {
 		this.gamePanel = gamePanel;
 	}
@@ -16,13 +17,19 @@ public class KeyboardInputs implements KeyListener{
 	@Override
 	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
+
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+
 	}
 
 	@Override
 	public void keyPressed(KeyEvent e) {
-		switch(e.getKeyCode()) {
-		
+
+		switch (e.getKeyCode()) {
 		case KeyEvent.VK_W:
 			gamePanel.changeYDelta(-5);
 			break;
@@ -36,12 +43,7 @@ public class KeyboardInputs implements KeyListener{
 			gamePanel.changeXDelta(5);
 			break;
 		}
-		
-	}
 
-	@Override
-	public void keyReleased(KeyEvent e) {
-		// TODO Auto-generated method stub
 	}
 
 }
