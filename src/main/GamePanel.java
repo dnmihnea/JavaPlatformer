318446c630e273029a33ee86b005c3db076f29cd
dnmihnea <mihnea.dinu100@gmail.com>
2022-10-10 16:44:51 +0100

    Revert "stash"
    This reverts commit 5752dbc14533970cfd952fc98a085e9f8b3f3d86.

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,8 +1,6 @@
 package main;
 
-import java.awt.Color;
 import java.awt.Graphics;
-import java.util.Random;
 
 import javax.swing.JPanel;
 
@@ -14,21 +12,11 @@ public class GamePanel extends JPanel{
 	private KeyboardInputs keyboardInputs;
 	private MouseInputs mouseInputs;
 	
-	private float xDelta = 100;
-	private float xDir = 1f;
-	private float yDelta = 100;
-	private float yDir = 1f;
-	
-	private int frames = 0;
-	private long lastCheck = 0;
-	
-	private Color color = new Color(150, 20, 90);
-	
-	private Random random;
+	private int xDelta = 100;
+	private int yDelta = 100;
 
 	public GamePanel() {
 		
-		random = new Random();
 		keyboardInputs = new KeyboardInputs(this);
 		mouseInputs = new MouseInputs(this);
 		addKeyListener(keyboardInputs); //placed keyboard inputs in its own class
@@ -38,45 +26,23 @@ public class GamePanel extends JPanel{
 	
 	public void changeXDelta(int value) {
 		this.xDelta += value;
+		repaint();
 	}
 	
 	public void changeYDelta(int value) {
 		this.yDelta += value;
+		repaint();
 	}
 	
 	public void setRectPos (int x, int y) {
 		this.xDelta = x;
 		this.yDelta = y;
+		repaint();
 	}
 	
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		
-		updateRectangle();
-		g.setColor(color);
-		g.fillRect((int)xDelta, (int)yDelta, 200, 50);
-
-	}
-
-	private void updateRectangle() {
-		xDelta += xDir;
-		if(xDelta > 400 || xDelta < 0) {
-			xDir*=-1;
-			color = getRandomColor();
-		}
-		yDelta += yDir;
-		if(yDelta > 400 || yDelta < 0) {
-			yDir*=-1;
-			color = getRandomColor();
-		}
-		
-	}
-
-	private Color getRandomColor() {
-		int r = random.nextInt(255);
-		int g = random.nextInt(255);
-		int b = random.nextInt(255);
-		
-		return new Color(r,g,b);
+		g.fillRect(xDelta, yDelta, 200, 50);
 	}
 }
